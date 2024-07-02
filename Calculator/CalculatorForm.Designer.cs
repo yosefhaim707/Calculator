b4d7bec22da3821a6eba5e6f6fbec5ff931f86a5
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-07-02 17:46:00 +0300

    Add project files.

---
+++ Calculator/CalculatorForm.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Calculator
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button_operatorEqual = new Button();
+            button_value1 = new Button();
+            button_value3 = new Button();
+            button_value2 = new Button();
+            button_value8 = new Button();
+            button_value9 = new Button();
+            button_value7 = new Button();
+            button_value5 = new Button();
+            button_value6 = new Button();
+            button_value4 = new Button();
+            button_value0 = new Button();
+            button_operatorMult = new Button();
+            button_operatorSub = new Button();
+            button_operatorDivision = new Button();
+            button_operatorAdd = new Button();
+            button19 = new Button();
+            textBox_result = new TextBox();
+            textBox_input1 = new TextBox();
+            textBox_input2 = new TextBox();
+            comboBox_operators = new ComboBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkSeaGreen;
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button1.Location = new Point(576, 160);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 66);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.DarkSeaGreen;
+            button15.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button15.Location = new Point(294, 160);
+            button15.Name = "button15";
+            button15.Size = new Size(134, 66);
+            button15.TabIndex = 14;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.DarkSeaGreen;
+            button16.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button16.Location = new Point(434, 160);
+            button16.Name = "button16";
+            button16.Size = new Size(134, 66);
+            button16.TabIndex = 15;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button_operatorEqual
+            // 
+            button_operatorEqual.BackColor = Color.DarkSeaGreen;
+            button_operatorEqual.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_operatorEqual.Location = new Point(224, 95);
+            button_operatorEqual.Name = "button_operatorEqual";
+            button_operatorEqual.Size = new Size(134, 60);
+            button_operatorEqual.TabIndex = 16;
+            button_operatorEqual.Text = "=";
+            button_operatorEqual.UseVisualStyleBackColor = false;
+            // 
+            // button_value1
+            // 
+            button_value1.BackColor = Color.DarkSeaGreen;
+            button_value1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value1.Location = new Point(574, 160);
+            button_value1.Name = "button_value1";
+            button_value1.Size = new Size(134, 66);
+            button_value1.TabIndex = 0;
+            button_value1.Text = "1";
+            button_value1.UseVisualStyleBackColor = false;
+            button_value1.Click += add_byButtons;
+            // 
+            // button_value3
+            // 
+            button_value3.BackColor = Color.DarkSeaGreen;
+            button_value3.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value3.Location = new Point(292, 160);
+            button_value3.Name = "button_value3";
+            button_value3.Size = new Size(134, 66);
+            button_value3.TabIndex = 2;
+            button_value3.Text = "3";
+            button_value3.UseVisualStyleBackColor = false;
+            button_value3.Click += add_byButtons;
+            // 
+            // button_value2
+            // 
+            button_value2.BackColor = Color.DarkSeaGreen;
+            button_value2.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value2.Location = new Point(432, 160);
+            button_value2.Name = "button_value2";
+            button_value2.Size = new Size(134, 66);
+            button_value2.TabIndex = 1;
+            button_value2.Text = "2";
+            button_value2.UseVisualStyleBackColor = false;
+            button_value2.Click += add_byButtons;
+            // 
+            // button_value8
+            // 
+            button_value8.BackColor = Color.DarkSeaGreen;
+            button_value8.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value8.Location = new Point(432, 304);
+            button_value8.Name = "button_value8";
+            button_value8.Size = new Size(134, 66);
+            button_value8.TabIndex = 7;
+            button_value8.Text = "8";
+            button_value8.UseVisualStyleBackColor = false;
+            button_value8.Click += add_byButtons;
+            // 
+            // button_value9
+            // 
+            button_value9.BackColor = Color.DarkSeaGreen;
+            button_value9.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value9.Location = new Point(292, 304);
+            button_value9.Name = "button_value9";
+            button_value9.Size = new Size(134, 66);
+            button_value9.TabIndex = 8;
+            button_value9.Text = "9";
+            button_value9.UseVisualStyleBackColor = false;
+            button_value9.Click += add_byButtons;
+            // 
+            // button_value7
+            // 
+            button_value7.BackColor = Color.DarkSeaGreen;
+            button_value7.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value7.Location = new Point(574, 304);
+            button_value7.Name = "button_value7";
+            button_value7.Size = new Size(134, 66);
+            button_value7.TabIndex = 6;
+            button_value7.Text = "7";
+            button_value7.UseVisualStyleBackColor = false;
+            button_value7.Click += add_byButtons;
+            // 
+            // button_value5
+            // 
+            button_value5.BackColor = Color.DarkSeaGreen;
+            button_value5.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value5.Location = new Point(432, 232);
+            button_value5.Name = "button_value5";
+            button_value5.Size = new Size(134, 66);
+            button_value5.TabIndex = 4;
+            button_value5.Text = "5";
+            button_value5.UseVisualStyleBackColor = false;
+            button_value5.Click += add_byButtons;
+            // 
+            // button_value6
+            // 
+            button_value6.BackColor = Color.DarkSeaGreen;
+            button_value6.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value6.Location = new Point(292, 232);
+            button_value6.Name = "button_value6";
+            button_value6.Size = new Size(134, 66);
+            button_value6.TabIndex = 5;
+            button_value6.Text = "6";
+            button_value6.UseVisualStyleBackColor = false;
+            button_value6.Click += add_byButtons;
+            // 
+            // button_value4
+            // 
+            button_value4.BackColor = Color.DarkSeaGreen;
+            button_value4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value4.Location = new Point(574, 232);
+            button_value4.Name = "button_value4";
+            button_value4.Size = new Size(134, 66);
+            button_value4.TabIndex = 3;
+            button_value4.Text = "4";
+            button_value4.UseVisualStyleBackColor = false;
+            button_value4.Click += add_byButtons;
+            // 
+            // button_value0
+            // 
+            button_value0.BackColor = Color.DarkSeaGreen;
+            button_value0.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_value0.Location = new Point(294, 376);
+            button_value0.Name = "button_value0";
+            button_value0.Size = new Size(414, 66);
+            button_value0.TabIndex = 9;
+            button_value0.Text = "0";
+            button_value0.UseVisualStyleBackColor = false;
+            button_value0.Click += add_byButtons;
+            // 
+            // button_operatorMult
+            // 
+            button_operatorMult.BackColor = Color.DarkSeaGreen;
+            button_operatorMult.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_operatorMult.Location = new Point(152, 376);
+            button_operatorMult.Name = "button_operatorMult";
+            button_operatorMult.Size = new Size(134, 66);
+            button_operatorMult.TabIndex = 28;
+            button_operatorMult.Text = "x";
+            button_operatorMult.UseVisualStyleBackColor = false;
+            button_operatorMult.Click += button_operatorActive;
+            // 
+            // button_operatorSub
+            // 
+            button_operatorSub.BackColor = Color.DarkSeaGreen;
+            button_operatorSub.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_operatorSub.Location = new Point(152, 232);
+            button_operatorSub.Name = "button_operatorSub";
+            button_operatorSub.Size = new Size(134, 66);
+            button_operatorSub.TabIndex = 27;
+            button_operatorSub.Text = "-";
+            button_operatorSub.UseVisualStyleBackColor = false;
+            button_operatorSub.Click += button_operatorActive;
+            // 
+            // button_operatorDivision
+            // 
+            button_operatorDivision.BackColor = Color.DarkSeaGreen;
+            button_operatorDivision.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_operatorDivision.Location = new Point(152, 304);
+            button_operatorDivision.Name = "button_operatorDivision";
+            button_operatorDivision.Size = new Size(134, 66);
+            button_operatorDivision.TabIndex = 26;
+            button_operatorDivision.Text = "/";
+            button_operatorDivision.UseVisualStyleBackColor = false;
+            button_operatorDivision.Click += button_operatorActive;
+            // 
+            // button_operatorAdd
+            // 
+            button_operatorAdd.BackColor = Color.DarkSeaGreen;
+            button_operatorAdd.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button_operatorAdd.Location = new Point(152, 160);
+            button_operatorAdd.Name = "button_operatorAdd";
+            button_operatorAdd.Size = new Size(134, 66);
+            button_operatorAdd.TabIndex = 24;
+            button_operatorAdd.Text = "+";
+            button_operatorAdd.UseVisualStyleBackColor = false;
+            button_operatorAdd.Click += button_operatorActive;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.DarkSeaGreen;
+            button19.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            button19.Location = new Point(154, 160);
+            button19.Name = "button19";
+            button19.Size = new Size(134, 66);
+            button19.TabIndex = 25;
+            button19.Text = "button19";
+            button19.UseVisualStyleBackColor = false;
+            // 
+            // textBox_result
+            // 
+            textBox_result.BackColor = Color.DarkSeaGreen;
+            textBox_result.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            textBox_result.Location = new Point(84, 98);
+            textBox_result.Name = "textBox_result";
+            textBox_result.Size = new Size(134, 55);
+            textBox_result.TabIndex = 29;
+            // 
+            // textBox_input1
+            // 
+            textBox_input1.BackColor = Color.DarkSeaGreen;
+            textBox_input1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            textBox_input1.Location = new Point(655, 95);
+            textBox_input1.Name = "textBox_input1";
+            textBox_input1.Size = new Size(134, 55);
+            textBox_input1.TabIndex = 30;
+            textBox_input1.Click += textBox_input1_Click;
+            textBox_input1.TextChanged += textBox_input1_TextChanged;
+            // 
+            // textBox_input2
+            // 
+            textBox_input2.BackColor = Color.DarkSeaGreen;
+            textBox_input2.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            textBox_input2.Location = new Point(364, 99);
+            textBox_input2.Name = "textBox_input2";
+            textBox_input2.Size = new Size(134, 55);
+            textBox_input2.TabIndex = 31;
+            textBox_input2.Click += textBox_input2_Click;
+            textBox_input2.TextChanged += textBox_input2_TextChanged;
+            // 
+            // comboBox_operators
+            // 
+            comboBox_operators.BackColor = Color.DarkSeaGreen;
+            comboBox_operators.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox_operators.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            comboBox_operators.FormattingEnabled = true;
+            comboBox_operators.Items.AddRange(new object[] { "+", "-", "/", "x" });
+            comboBox_operators.Location = new Point(504, 98);
+            comboBox_operators.Name = "comboBox_operators";
+            comboBox_operators.Size = new Size(136, 56);
+            comboBox_operators.TabIndex = 32;
+            comboBox_operators.SelectedIndex = 0;
+            // 
+            // CalculatorForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(823, 528);
+            Controls.Add(comboBox_operators);
+            Controls.Add(textBox_input2);
+            Controls.Add(textBox_input1);
+            Controls.Add(textBox_result);
+            Controls.Add(button_operatorMult);
+            Controls.Add(button_operatorSub);
+            Controls.Add(button_operatorDivision);
+            Controls.Add(button_operatorAdd);
+            Controls.Add(button19);
+            Controls.Add(button_value0);
+            Controls.Add(button_value5);
+            Controls.Add(button_value6);
+            Controls.Add(button_value4);
+            Controls.Add(button_value8);
+            Controls.Add(button_value9);
+            Controls.Add(button_value7);
+            Controls.Add(button_operatorEqual);
+            Controls.Add(button_value2);
+            Controls.Add(button16);
+            Controls.Add(button_value3);
+            Controls.Add(button15);
+            Controls.Add(button_value1);
+            Controls.Add(button1);
+            Name = "CalculatorForm";
+            RightToLeft = RightToLeft.Yes;
+            RightToLeftLayout = true;
+            Text = "CalculatorForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button15;
+        private Button button16;
+        private Button button_operatorEqual;
+        private Button button_value1;
+        private Button button_value3;
+        private Button button_value2;
+        private Button button_value8;
+        private Button button_value9;
+        private Button button_value7;
+        private Button button_value5;
+        private Button button_value6;
+        private Button button_value4;
+        private Button button_value0;
+        private Button button_operatorMult;
+        private Button button_operatorSub;
+        private Button button_operatorDivision;
+        private Button button_operatorAdd;
+        private Button button19;
+        private TextBox textBox_result;
+        private TextBox textBox_input1;
+        private TextBox textBox_input2;
+        private ComboBox comboBox_operators;
+    }
+}
---
+++ Calculator/CalculatorForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private TextBox _ActiveTextBox;
+        public CalculatorForm()
+        {
+            InitializeComponent();
+            _ActiveTextBox = textBox_input1;
+        }
+
+        private void textBox_input1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_input1_Click(object sender, EventArgs e)
+        {
+            _ActiveTextBox = (TextBox)sender;
+        }
+
+        private void textBox_input2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_input2_Click(object sender, EventArgs e)
+        {
+            _ActiveTextBox = (TextBox)sender;
+        }
+
+        private void button_operatorActive(object sender, EventArgs e)
+        {
+            comboBox_operators.SelectedItem = ((Button)sender).Text;
+        }
+        private void add_byButtons(object sender, EventArgs e)
+        {
+            _ActiveTextBox.Text += ((Button)sender).Text;
+        }
+    }
+}
